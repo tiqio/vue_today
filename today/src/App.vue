901f6a219c4f3b965b390730c8290e450a02fdd6
HP <215572162@qq.com>
2020-10-27 07:24:29 +0800

    vue.json

--- today/src/App.vue
+++ today/src/App.vue
@@ -11,6 +11,9 @@ export default {
   name: 'App',
   components: {
     today
+  },
+  methods:{
+    
   }
 }
 </script>
--- today/src/components/today.vue
+++ today/src/components/today.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
+  <div class='a'>
+    sss
     
+  <button @click="ss">ss</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "today"
+  name: "today",
+  methods:{
+    
+    ss(){
+      console.log('ss');
+    }
+  },
+  mounted(){
+      console.log("测试");
+  }
 }
 </script>
 
 <style scoped>
-
+  .a{
+    color: gold;
+  }
 </style>
